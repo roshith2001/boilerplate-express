48bbc80b388ef899d05496ce30f25181e9fa8722
Roshith Krishna P <roshithj@gmail.com>
2024-07-20 16:36:52 +0000

    5

--- myApp.js
+++ myApp.js
@@ -1,5 +1,26 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
+app.use('/public', express.static(__dirname+'/public'));
+app.use(function middleware(req, res, next){
+    var string = req.method+" "+req.path+" - "+req.ip;
+    console.log(string);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/views/index.html')
+})
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message": "HELLO JSON"})
+    }
+    else{
+        res.json({"message": "Hello json"})
+    }
+})
+
+console.log("Hello World")
 
 
 
